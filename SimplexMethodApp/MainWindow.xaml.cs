e7603e6058af7a670f945fb706e8bd29f8e96211
DJ3epo <nikitich1107@gmail.com>
2025-04-13 23:44:51 +0300

    Добавьте файлы проекта.

---
+++ SimplexMethodApp/MainWindow.xaml.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace SimplexMethodApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SolveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Получаем данные с текстовых полей
+                string[] AStrings = TextBoxA.Text.Split(';');
+                string[] bStrings = TextBoxB.Text.Split(';');
+                string[] cStrings = TextBoxC.Text.Split(';');
+
+                // Проверка: матрица A
+                int m = AStrings.Length;
+                int n = AStrings[0].Split(',').Length;
+
+                // Проверка: размерность вектора b
+                if (bStrings.Length != m)
+                {
+                    MessageBox.Show($"Количество строк в A ({m}) не совпадает с размерностью вектора b ({bStrings.Length})");
+                    return;
+                }
+
+                // Проверка: размерность вектора c
+                if (cStrings.Length != n)
+                {
+                    MessageBox.Show($"Количество столбцов в A ({n}) не совпадает с размерностью вектора c ({cStrings.Length})");
+                    return;
+                }
+
+                // Создание матрицы A
+                double[,] A = new double[m, n];
+                for (int i = 0; i < m; i++)
+                {
+                    string[] row = AStrings[i].Split(',');
+                    for (int j = 0; j < n; j++)
+                    {
+                        A[i, j] = double.Parse(row[j]);
+                    }
+                }
+
+                // Создание вектора b
+                double[] b = new double[m];
+                for (int i = 0; i < m; i++)
+                {
+                    b[i] = double.Parse(bStrings[i]);
+                }
+
+                // Создание вектора c
+                double[] c = new double[n];
+                for (int i = 0; i < n; i++)
+                {
+                    c[i] = double.Parse(cStrings[i]);
+                }
+
+                // Решение задачи с помощью метода Симплекса
+                var result = SimplexMethod(A, b, c);
+
+                // Вывод результата
+                ResultTextBox.Text = $"Оптимальное решение: {string.Join(", ", result.Item1)}\n";
+                ResultTextBox.Text += $"Значение целевой функции: {result.Item2}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+
+        // Метод решения задачи методом Симплекса
+        public Tuple<double[], double> SimplexMethod(double[,] A, double[] b, double[] c)
+        {
+            int m = b.Length;
+            int n = c.Length;
+
+            // Инициализация симплекс-таблицы
+            double[,] tableau = new double[m + 1, n + m + 1];
+
+            // Заполнение столбца правых частей
+            for (int i = 0; i < m; i++)
+            {
+                tableau[i, n + m] = b[i];
+            }
+
+            // Заполнение таблицы для A
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    tableau[i, j] = A[i, j];
+                }
+            }
+
+            // Заполнение целевой функции (с учетом знака)
+            for (int i = 0; i < n; i++)
+            {
+                tableau[m, i] = -c[i]; // Минус для максимизации
+            }
+
+            // Симплексный метод
+            while (true)
+            {
+                // Поиск столбца с наибольшим отрицательным значением в последней строке
+                int pivotCol = -1;
+                double minVal = 0;
+                for (int i = 0; i < n + m; i++)
+                {
+                    if (tableau[m, i] < minVal)
+                    {
+                        minVal = tableau[m, i];
+                        pivotCol = i;
+                    }
+                }
+
+                if (pivotCol == -1) break; // Оптимальное решение найдено
+
+                // Поиск строки для поворота
+                int pivotRow = -1;
+                double minRatio = double.MaxValue;
+                for (int i = 0; i < m; i++)
+                {
+                    if (tableau[i, pivotCol] > 0)
+                    {
+                        double ratio = tableau[i, n + m] / tableau[i, pivotCol];
+                        if (ratio < minRatio)
+                        {
+                            minRatio = ratio;
+                            pivotRow = i;
+                        }
+                    }
+                }
+
+                // Поворот таблицы
+                double pivot = tableau[pivotRow, pivotCol];
+                for (int i = 0; i < n + m + 1; i++)
+                {
+                    tableau[pivotRow, i] /= pivot;
+                }
+
+                for (int i = 0; i < m + 1; i++)
+                {
+                    if (i != pivotRow)
+                    {
+                        double factor = tableau[i, pivotCol];
+                        for (int j = 0; j < n + m + 1; j++)
+                        {
+                            tableau[i, j] -= factor * tableau[pivotRow, j];
+                        }
+                    }
+                }
+            }
+
+            // Извлечение решения из таблицы
+            double[] solution = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                solution[i] = 0;
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                if (tableau[i, n + m] != 0)
+                {
+                    solution[i] = tableau[i, n + m];
+                }
+            }
+
+            // Целевая функция
+            double objectiveValue = tableau[m, n + m];
+
+            return Tuple.Create(solution, objectiveValue);
+        }
+    }
+}
---
+++ SimplexMethodApp/Simplex.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+public class SimplexMethod
+{
+    public static (double[], double) Solve(double[,] A, double[] b, double[] c)
+    {
+        int m = A.GetLength(0);
+        int n = A.GetLength(1);
+
+        // Создание начальной симплекс-таблицы
+        double[,] table = new double[m + 1, n + m + 1];
+
+        // Заполнение таблицы
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                table[i, j] = A[i, j];
+            }
+            table[i, n + i] = 1;  // единичные элементы для базисных переменных
+            table[i, n + m] = b[i]; // правые части
+        }
+
+        // Заполнение последней строки с целевой функцией
+        for (int j = 0; j < n; j++)
+        {
+            table[m, j] = -c[j];
+        }
+
+        // Симплекс-метод
+        while (true)
+        {
+            // Проверка на оптимальность
+            int pivotCol = -1;
+            for (int j = 0; j < n + m; j++)
+            {
+                if (table[m, j] < 0)
+                {
+                    pivotCol = j;
+                    break;
+                }
+            }
+
+            // Если все элементы последней строки >= 0, задача решена
+            if (pivotCol == -1) break;
+
+            // Находим строку для поворота
+            int pivotRow = -1;
+            double minRatio = double.MaxValue;
+
+            for (int i = 0; i < m; i++)
+            {
+                if (table[i, pivotCol] > 0)
+                {
+                    double ratio = table[i, n + m] / table[i, pivotCol];
+                    if (ratio < minRatio)
+                    {
+                        minRatio = ratio;
+                        pivotRow = i;
+                    }
+                }
+            }
+
+            if (pivotRow == -1)
+                throw new Exception("Задача неограничена!");
+
+            // Поворот (пивотирование)
+            Pivot(table, pivotRow, pivotCol);
+        }
+
+        // Получаем решение
+        double[] solution = new double[n];
+        for (int i = 0; i < m; i++)
+        {
+            if (table[i, n + i] == 1)
+            {
+                solution[i] = table[i, n + m];
+            }
+            else
+            {
+                solution[i] = 0;
+            }
+        }
+
+        double objectiveValue = table[m, n + m];
+        return (solution, objectiveValue);
+    }
+
+    private static void Pivot(double[,] table, int pivotRow, int pivotCol)
+    {
+        int m = table.GetLength(0) - 1;
+        int n = table.GetLength(1) - 1;
+
+        double pivotValue = table[pivotRow, pivotCol];
+        // Делим всю строку на элемент в pivot
+        for (int j = 0; j <= n; j++)
+        {
+            table[pivotRow, j] /= pivotValue;
+        }
+
+        // Обновляем все остальные строки
+        for (int i = 0; i <= m; i++)
+        {
+            if (i != pivotRow)
+            {
+                double factor = table[i, pivotCol];
+                for (int j = 0; j <= n; j++)
+                {
+                    table[i, j] -= factor * table[pivotRow, j];
+                }
+            }
+        }
+    }
+
+    private static void PrintTable(double[,] table)
+    {
+        int m = table.GetLength(0) - 1;
+        int n = table.GetLength(1) - 1;
+
+        for (int i = 0; i <= m; i++)
+        {
+            for (int j = 0; j <= n; j++)
+            {
+                Console.Write(table[i, j].ToString("F2") + "\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
